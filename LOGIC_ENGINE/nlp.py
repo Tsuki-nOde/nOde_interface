32b44dd035d433726260f007496787d59fdb0acf
Tsuki n0de <n0de.tsuki@gmail.com>
2025-07-21 17:59:21 +0530

    Fix Some Error

--- LOGIC_ENGINE/nlp.py
+++ LOGIC_ENGINE/nlp.py
@@ -5,50 +5,36 @@ from spacy.matcher import PhraseMatcher
 
 nlp = spacy.load("en_core_web_md")
 
-with open("./JSON_LOUNGE/DATA.JSON" , "r") as f:
+with open("./JSON_LOUNGE/DATA.JSON", "r") as f:
     intentions = json.load(f)["intents"]
 
 with open("./JSON_LOUNGE/dataresponse.json", "r") as f:
     response = json.load(f)["responses"]
 
+matcher = PhraseMatcher(nlp.vocab, attr="LOWER")
 
-
-
-
-matcher = PhraseMatcher(nlp.vocab, attr = "lower")
-
-intent_map = {}
-
+# Build matcher
 for intent in intentions:
     tag = intent["tag"]
-    patterns = [nlp.make_doc(p) for p  in intent["patterns"]]
+    patterns = [nlp.make_doc(p) for p in intent["patterns"]]
     matcher.add(tag, patterns)
-    for phrase in intent["patterns"]:
-        intent_map[phrase.lower()] = tag
-
 
 def detect_intent(user_input):
     doc = nlp(user_input)
     matches = matcher(doc)
 
-    matched_span = ""
-    match_id = None
-
     if matches:
-        match_id,start,end = matches[0]
-        matched_span = doc[start:end].text.lower()
-
-
-        for phrase, tag in intent_map.items():
-            if phrase in matched_span:
-                return tag
-        
-        return nlp.vocab.strings[match_id]
+        match_id, start, end = matches[0]
+        tag = nlp.vocab.strings[match_id]
+        return tag
     else:
         return "unknown"
 
 def get_response(tag):
     if tag in response:
         return random.choice(response[tag])
-        
+    else:
+        return "ちょっと分からないけど…もう一度言ってくれる？"
 
+if __name__ == "__main__":
+    pass
--- MAIN_MODULE/main.py
+++ MAIN_MODULE/main.py
@@ -9,17 +9,17 @@ from LOGIC_ENGINE import nlp
 def chat():
     print("あの月さん!! ....... I am online!!!!")
 
-while True:
-    user_input = input("You : ")
+    while True:
+        user_input = input("You : ")
     
     
-    if user_input.lower() in ["later", "bye"]:
+        if user_input.lower() in ["later", "bye"]:
             print("Waguri : Leaving already  ?? また後で!!")
             break
 
-    tag = nlp.detect_intent(user_input)
-    response = nlp.get_response(tag)
-    print(f"Waguri : {response}")
+        tag = nlp.detect_intent(user_input)
+        response = nlp.get_response(tag)
+        print(f"Waguri : {response}")
 
 if __name__ =="__main__":
     chat()
