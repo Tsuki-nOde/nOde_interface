d492eea4f705f6230ea644591e88951ff1ffa718
Tsuki n0de <n0de.tsuki@gmail.com>
2025-07-21 09:24:52 +0530

    initial commit

---
+++ MAIN_MODULE/main.py
@@ -0,0 +1,25 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+
+from LOGIC_ENGINE import nlp
+
+def chat():
+    print("あの月さん!! ....... I am online!!!!")
+
+while True:
+    user_input = input("You : ")
+    
+    
+    if user_input.lower() in ["later", "bye"]:
+            print("Waguri : Leaving already  ?? また後で!!")
+            break
+
+    tag = nlp.detect_intent(user_input)
+    response = nlp.get_response(tag)
+    print(f"Waguri : {response}")
+
+if __name__ =="__main__":
+    chat()
