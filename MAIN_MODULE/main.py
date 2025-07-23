6175163544e5f1f48d8614b1346bef6da583625e
Tsuki n0de <n0de.tsuki@gmail.com>
2025-07-24 00:52:36 +0530

    changed main.py to more structured

--- MAIN_MODULE/main.py
+++ MAIN_MODULE/main.py
@@ -1,35 +1,50 @@
 import sys
 import os
+
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
-from API_LOUNGE import weather
+from API_LOUNGE import weather, jokes_api
 from LOGIC_ENGINE import nlp
+
+
+def handle_weather():
+    base_response = nlp.get_response("weather")
+    weather_report = weather.get_weather()
+    return f"{base_response} {weather_report}"
+
+def handle_joke():
+    base_response = nlp.get_response("joke")
+    joke = jokes_api.get_joke()
+    return f"{base_response}, {joke}"
+
+def handle_general(tag):
+    return nlp.get_response(tag)
 
 
 def chat():
     print("あの月さん!! ....... I am online!!!!")
 
     while True:
-        user_input = input("You : ")
-    
-        if user_input.lower() in ["later", "bye"]:
-            print("Waguri : Leaving already  ?? また後で!!")    
-            break
+        user_input = input("You : ").strip()
 
+        if user_input.lower() in ["later", "bye"]:
+            print("Waguri : Leaving already?? また後で!!")    
+            break
 
         tag = nlp.detect_intent(user_input)
 
+        
         if tag == "weather":
-            base_response = nlp.get_response(tag)
-            weather_report = weather.get_weather()
-            response = f"{base_response} {weather_report}"
-
+            response = handle_weather()
+        elif tag == "joke":
+            response = handle_joke()
         else:
-            response = nlp.get_response(tag)
+            response = handle_general(tag)
 
         print(f"Waguri : {response}")
 
-
-if __name__ =="__main__":
+# Entry Point
+if __name__ == "__main__":
     chat()
